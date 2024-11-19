0a667caf2b048b43f719155259ec640a1a51454d
dija-prog <157844481+dija-prog@users.noreply.github.com>
2024-11-19 10:11:54 +0000

    g

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$bd_host = "zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$db_user = "hpwdlkk6ytzu2iaz";
+$db_pass= "l3mij96rgvsf75se";
+$db_name = "qj0hr19ajswaa9v1";
+
+$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die ("database connection error");
+?>
