a2e333e064ef9cc4096d940c3fcc2eae96c60710
dija-prog <khadija_araai@outlook.fr>
2024-12-09 15:12:47 +0100

    update

--- config.php
+++ config.php
@@ -4,17 +4,20 @@
 // $url =getenv('mysql://hpwdlkk6ytzu2iaz:l3mij96rgvsf75se@zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/qj0hr19ajswaa9v1
 // ');
 $url =getenv('JAWSDB_URL');
+echo $url;
 $db = parse_url($url);
+
 
 // $host =$db['zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
 // $port =$db['3306'];
 // $username = $db['hpwdlkk6ytzu2iaz'];
 // $password = $db['l3mij96rgvsf75se'];
 // $dbname = ltrim($db['qj0hr19ajswaa9v1'], '/');
-$host =$db['host'];
-$username = $db['user'];
-$password = $db['pass'];
-$dbname = ltrim($db['path'], '/');
+$host = 'zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username = 'hpwdlkk6ytzu2iaz';
+$password = 'l3mij96rgvsf75se';
+$port ='3306';
+$dbname = '	qj0hr19ajswaa9v1';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username,$password);
