27ea1ece6ccde48059c8fd666ddde9922ef07a90
dija-prog <khadija_araai@outlook.fr>
2025-07-11 11:00:16 +0200

    mise à jour

--- src/Controllers/AccueilController.php
+++ src/Controllers/AccueilController.php
@@ -1,21 +1,25 @@
 <?php
 namespace App\Controllers;
+
 use App\Models\AvisModel;
 
 class AccueilController
-{   
+{
     private $avisModel;
-    
-    public function index()
+
+    public function __construct()
     {
-        $avisModel = new AvisModel();
-        $avisInsert = $avisModel -> addAvis();
-        $avisValides= $avisModel ->getAvisValides();
-
-
-        //inclure la vue avec les données 
-    
-        require_once __DIR__ . '/../views/Accueil.php';
+        // Initialisation de la propriété
+        $this->avisModel = new AvisModel();
     }
 
+    public function index()
+    {
+        // Utilisation de la propriété initialisée
+        $avisInsert = $this->avisModel->addAvis();
+        $avisValides = $this->avisModel->getAvisValides();
+
+        // Inclure la vue avec les données
+        require_once __DIR__ . '/../views/Accueil.php';
+    }
 }
