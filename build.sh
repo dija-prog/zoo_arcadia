9cf90d4f125f357c759beb5836ec23243ba5d071
dija-prog <khadija_araai@outlook.fr>
2025-07-12 23:12:24 +0200

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
-
 #!/bin/bash
-curl -sS https://getcomposer.org/installer | php
-php composer.phar install --no-dev --optimize-autoloader
-cp -r vendor/ public/vendor/ 2>/dev/null || :
-chmod -R 755 vendor
+# Version corrigée
+set -e # Arrête le script en cas d'erreur
+
+# Installer Composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Installer les dépendances
+composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
+
+# Corriger les permissions
+chmod -R 755 /var/www/html/vendor
+chown -R www-data:www-data /var/www/html/vendor
+
+# Solution alternative pour l'autoload
+if [ ! -f "/var/www/html/vendor/autoload.php" ]; then
+    ln -s /var/www/html/vendor /var/www/html/public/vendor
+fi
