fb43152598cd3869a98b0f92c8a3428c41fb79e1
dija-prog <khadija_araai@outlook.fr>
2025-09-07 18:48:42 +0200

    index

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
 
 // En prod → cacher les erreurs
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 
 // Les erreurs vont dans le log, pas à l’écran
 ini_set('log_errors', 1);
